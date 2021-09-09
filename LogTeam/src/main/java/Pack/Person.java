1938209deb221062df14dfa08427c6729462c222
20131827 <90309088+20131827@users.noreply.github.com>
2021-09-09 19:04:36 +0900

    쉬운버전
    쉬운버전

---
+++ LogTeam/src/main/java/Pack/Person.java
@@ -0,0 +1,60 @@
+package Pack;
+
+
+
+public class Person {
+	String id;
+	String pw;
+	String nname;
+	String addr;
+	String tel;
+	String email;
+	
+	
+	public Person() {
+		System.out.println("생성자 호출");
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNname() {
+		return nname;
+	}
+	public void setNname(String nname) {
+		this.nname = nname;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+	
+}
